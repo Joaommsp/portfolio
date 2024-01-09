80a40f54336d1d4377dfee36d48bc29270166a29
Joao <jmmsp2003@hotmail.com>
2024-01-09 17:37:09 -0300

    update 09/01

--- assets/script/scrollReveal.js
+++ assets/script/scrollReveal.js
@@ -6,8 +6,9 @@ ScrollReveal().reveal('.dropDown', { duration: 1000, distance: '100px', easing: 
 // gitHub
 ScrollReveal().reveal('.githubData', { duration: 1000, distance: '100px', easing: 'ease', reset: true });
 
+// projetos
+ScrollReveal().reveal('.projects', { duration: 1000, distance: '100px', easing: 'ease', reset: true });
+
 // cursos e formações
 ScrollReveal().reveal('.courses', { duration: 1000, distance: '100px', easing: 'ease', reset: true });
 ScrollReveal().reveal('.bootcamps', { duration: 1000, distance: '100px', easing: 'ease', reset: true });
-
-
